26b4e1b364d0d6548511dfca0303e5b90606e099
Roman <Slem2008@inbox.ru>
2022-08-28 13:47:03 +0300

    Добавлние в список

---
+++ Урок6/task46.py
@@ -0,0 +1,10 @@
+# Добавление в список.
+elements = []
+elements.append("a")
+elements.append(1) # Если записать цифру в "7" то она примет строковое значение
+elements.append(7)
+elements.append("b")
+
+
+print(elements)
+
